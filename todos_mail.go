eeda1be7c7a645a5e48cd4c9ea11062206172ee4
‘곽율희’ <yulhee.kwak@rsquare.co.kr>
2018-03-05 11:24:20 +0900

    todos mutation 추가

--- todos_mail.go
+++ todos_mail.go
@@ -1,19 +1,39 @@
 package rmail
 
-func (client *Client) CreateTodos(todoId int) {
+func (client *Client) CreateTodos(todoIds []int){
 	query := `
-		mutation sendTodoMail($id: Int!) {
-			send_mail {
-				covenants {
-					create_covenant(covenant_id: $id) {
-						shardId
-					}
+		mutation sendTodoMail($ids: [Int!]!){
+			send_mail{
+			todos{
+				add_todos(todo_ids: $ids)
+				}
+			}
+		}
+	`
+	vars := map[string]interface{}{
+		"ids": todoIds,
+	}
+
+	client.sendMail(query, vars)
+	
+}
+
+func (client *Client) UpdateTodos(todoId int, oldTodo map[string]interface{}, userIds []int) {
+	query := `
+		mutation sendTodoMail($id: Int!, $old_Todo: Todo!, $added_user_id: [Int!]){
+			send_mail{
+				todos{
+					update_todo(todo_id: $id
+					old_todo: $old_Todo
+					added_user_id: $added_user_id)
 				}
 			}
 		}
 	`
 	vars := map[string]interface{}{
 		"id": todoId,
+		"old_Todo": oldTodo,
+		"added_user_id": userIds,
 	}
 
 	client.sendMail(query, vars)
