a891f5ca25d5abf27cceb0218c2d08506c4051a3
‘곽율희’ <yulhee.kwak@rsquare.co.kr>
2018-03-07 14:56:52 +0900

    딜의 팁 추가

--- tips_mail.go
+++ tips_mail.go
@@ -1,6 +1,6 @@
 package rmail
 
-func (client *Client) CreateTips(tipId int){
+func (client *Client) CreateClientTip(tipId int){
 	query := `
 		mutation sendTodoMail($id: Int!){
 			send_mail{
@@ -14,4 +14,21 @@ func (client *Client) CreateTips(tipId int){
 		"id": tipId,
 	}
 	client.sendMail(query, vars)
+}
+
+func (client *Client) CreateDealTip(tipId int) {
+	query := `
+		mutation sendTodoMail($id: Int!){
+			send_mail{
+			tips{
+				add_tip_to_deal(tip_id: $id)
+				}
+			}
+		}
+	`
+	vars := map[string]interface{}{
+		"id": tipId,
+	}
+	client.sendMail(query, vars)
+
 }
