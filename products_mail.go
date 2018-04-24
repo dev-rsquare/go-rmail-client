96ad3b154f5652d2a4815652cbcc917c2aedd2fb
drskur <drskur@me.com>
2018-04-24 14:27:45 +0900

    ManageProductDelete

--- products_mail.go
+++ products_mail.go
@@ -15,3 +15,19 @@ func (client *Client) ManageProductCreate(productId int) {
 	}
 	client.sendMail(query, vars)
 }
+
+func (client *Client) ManageProductDelete(productId int) {
+	query := `
+		mutation sendDeleteProductMail($id: Int!) {
+			send_mail {
+				products {
+					delete_product(product_id: $id)
+				}
+			}
+		}
+	`
+	vars := map[string]interface{}{
+		"id": productId,
+	}
+	client.sendMail(query, vars)
+}
