0a5dcf087b0ccda8966bd7f02d5e4af51b6f7d57
drskur <drskur@me.com>
2018-04-27 15:31:21 +0900

    Add TakePhotos Mail

--- tasks_mail.go
+++ tasks_mail.go
@@ -5,8 +5,8 @@ func (client *Client) ChangeTaskEvent(taskID int, event string) {
 	query := `
 		mutation sendChangeTaskEventMail($id: Int!, $event: String!){
 			send_mail{
-			tasks{
-				change_task_event(task_id: $id, event: $event)
+				tasks {
+					change_task_event(task_id: $id, event: $event)
 				}
 			}
 		}
@@ -14,6 +14,23 @@ func (client *Client) ChangeTaskEvent(taskID int, event string) {
 	vars := map[string]interface{}{
 		"id":    taskID,
 		"event": event,
+	}
+	client.sendMail(query, vars)
+}
+
+// TakePhotosTask : send mail when created task for taking photos
+func (client *Client) TakePhotosTask(taskID int) {
+	query := `
+		mutation sendTakePhotosMail($taskId: Int!){
+			send_mail{
+				tasks {
+					take_photos(task_id: $taskId)
+				}
+			}
+		}
+	`
+	vars := map[string]interface{}{
+		"taskId":    taskID,
 	}
 	client.sendMail(query, vars)
 }
