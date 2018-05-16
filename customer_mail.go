b815ebb6d37421934021e09eecbd28c1c54438dd
drskur <drskur@me.com>
2018-05-16 17:58:43 +0900

    Sleep Customer

--- customer_mail.go
+++ customer_mail.go
@@ -2,6 +2,25 @@ package rmail
 
 import "fmt"
 
+// SleepCustomer : send mail when customer become sleep (dormant)
+func (client *Client) SleepCustomer(email string) {
+	query := `
+		mutation sendSleepCustomerMail($email: String!) {
+			send_mail {
+				customers {
+					sleep_customer(email: $email)
+				}
+			}
+		}
+	`
+	vars := map[string]interface{}{
+		"email": email,
+	}
+
+	client.sendMail(query, vars)
+}
+
+
 // AdjustCustomerLevel : send mail when changed customer's level
 func (client *Client) AdjustCustomerLevel(clientID int, level int) {
 	queryTemplate := `
